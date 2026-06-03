c43a65e34cb3664737391b331e64e5c77e876133
Priyavrat Talyan <ptalyan@apisod.ai>
2026-06-03 15:44:20 +0530

    Added core/UiState.kt

---
+++ app/src/main/java/com/apisod/inventoryar/core/result/UiState.kt
@@ -0,0 +1,18 @@
+package com.apisod.inventoryar.core.result
+
+sealed interface UiState<out T> {
+
+    data object Idle : UiState<Nothing>
+
+    data object Loading : UiState<Nothing>
+
+    data class Success<T>(
+        val data: T
+    ) : UiState<T>
+
+    data class Error(
+        val message: String
+    ) : UiState<Nothing>
+
+    data object Empty : UiState<Nothing>
+}
